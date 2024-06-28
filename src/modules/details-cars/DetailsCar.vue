c27dd5e390946beb6d33aace733d6ca6e930d5bf
AntonioCarri11o <20203tn078@utez.edu.mx>
2024-06-28 05:49:24 -0600

    avance details auto

--- src/modules/details-cars/DetailsCar.vue
+++ src/modules/details-cars/DetailsCar.vue
@@ -1,13 +1,85 @@
 <template lang="">
-    <div>
-        holiwis :3
-    </div>
+    <v-container>
+        <div>
+            <v-breadcrumbs
+            large
+            :items="items"
+            divider=">"
+            >
+            </v-breadcrumbs>
+        </div>
+        <v-row>
+            <v-col sm="12" md="6">
+                <v-img :src="'https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg'" height="280px" contain></v-img>                
+            </v-col>
+            <v-col sm="12" md="6" >
+                <v-card elevation="0" class="info-card">
+                    <h1 class="heading">{{ exampleVehicle.model }}</h1>
+                    <div class="rate-container my-4" >
+                        <v-icon
+                        large
+                        color="yellow"
+                        >
+                            mdi-star
+                        </v-icon>
+                        <h3 class="rate-font ms-1">
+                            {{ rate }} / 5.0
+                        </h3>
+                    </div>
+                    <h2>
+                        {{ exampleVehicle.price }}
+                    </h2>
+                </v-card>
+            </v-col>
+        </v-row>
+    </v-container>
 </template>
 <script>
 export default {
-    
+    name: 'DetailsCar',
+    data() {
+        return {
+            items: [
+                {
+                    text: 'Explorar',
+                    disabled: false,
+                    href: '/vehicles_search'
+                },
+                {
+                    text: 'Detalles',
+                    disabled: true,
+                }          
+            ],
+            exampleVehicle:
+            {
+                id: 1,
+                model: 'New SM5',
+                brand: 'Toyota',
+                price: '$79,999.99',
+                description: 'New SM5 nos da una probadita de su nuevo lanzamiento en México...',
+                image: 'https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg'
+            },
+            rate: 4.8
+        }
+    }
 }
 </script>
-<style lang="">
-    
+<style scoped>
+.heading {
+    color: #22272F;
+    size: 3rem;
+    font-weight: 450;
+}
+.rate-container {
+    display: flex;
+    align-items: center;
+}
+.rate-font {
+    color: #777A8B;    
+}
+.info-card {
+    display: flex;
+    flex-direction: column;
+    justify-content: start;    
+}
 </style>
