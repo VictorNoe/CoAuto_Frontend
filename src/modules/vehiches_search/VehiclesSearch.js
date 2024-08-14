a758609389ce8132ad29d17798d7882a35407847
ChristianAlonso16 <20213tn050@utez.edu.mx>
2024-08-14 16:50:23 -0600

    [UPD] Paths to register and search vehicles services

--- src/modules/vehiches_search/VehiclesSearch.js
+++ src/modules/vehiches_search/VehiclesSearch.js
@@ -1,6 +1,6 @@
 import apiGateway from '../../utils/Http.gateway';
 
-const API_URL_GETALLCARS = 'https://fohggt0wwd.execute-api.us-east-1.amazonaws.com/Prod/get_data';
+const API_URL_GETALLCARS = 'https://syr3x8vt0g.execute-api.us-east-1.amazonaws.com/Prod/get_data';
 
 const getAllCars = async() => {
     try {
